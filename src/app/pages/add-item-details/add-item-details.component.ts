3662b5f9e051f7fd353c13cc360eae1f99b1f0ad
BLasan <benuraab@gmail.com>
2019-08-05 03:04:15 +0530

    form validation and mongodb setup added

---
+++ src/app/pages/add-item-details/add-item-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-item-details',
+  templateUrl: './add-item-details.component.html',
+  styleUrls: ['./add-item-details.component.scss']
+})
+export class AddItemDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
