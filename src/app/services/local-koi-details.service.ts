be3e34d22d39c048f325e5a2e531e9e5ada00614
Amanda <thirunihp@gmail.com>
2019-09-07 20:18:28 +0530

    unit testing-customer-home

--- src/app/services/local-koi-details.service.ts
+++ src/app/services/local-koi-details.service.ts
@@ -19,4 +19,10 @@ export class LocalKoiDetailsService {
         var response = new SyncRequestClient().addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NDc2OTg1MzgsIm5iZiI6MTU0NzY5NDIxOCwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZSI6InN0cmluZyIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6InN0cmluZyIsIkRPQiI6IjEvMTcvMjAxOSIsImlzcyI6InlvdXIgYXBwIiwiYXVkIjoidGhlIGNsaWVudCBvZiB5b3VyIGFwcCJ9.qxFdcdAVKG2Idcsk_tftnkkyB2vsaQx5py1KSMy3fT4").get<Response>(url);
         return response;
     }
+
+    laodVisibility(){
+        let url = `http://localhost:4600/load_visibility_fish`;
+        var response = new SyncRequestClient().addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NDc2OTg1MzgsIm5iZiI6MTU0NzY5NDIxOCwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZSI6InN0cmluZyIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6InN0cmluZyIsIkRPQiI6IjEvMTcvMjAxOSIsImlzcyI6InlvdXIgYXBwIiwiYXVkIjoidGhlIGNsaWVudCBvZiB5b3VyIGFwcCJ9.qxFdcdAVKG2Idcsk_tftnkkyB2vsaQx5py1KSMy3fT4").get<Response>(url);
+        return response;
+    }
 }
--- src/scripts/visibility.js
+++ src/scripts/visibility.js
@@ -1,52 +1,52 @@
 export function visibility(){
     $(document).on('click','#change_name',function(e){
         e.preventDefault();
-        alert('po1')
+        // alert('po1')
     });
     
     $(document).on('click','#change_category',function(e){
         e.preventDefault();
-        alert('po2')
+        // alert('po2')
     });
     
     $(document).on('click','#change_code',function(e){
         e.preventDefault();
-        alert('po3')
+        // alert('po3')
     });
     
     $(document).on('click','#change_age',function(e){
         e.preventDefault();
-        alert('po4')
+        // alert('po4')
     });
     
     $(document).on('click','#change_size',function(e){
         e.preventDefault();
-        alert('po5')
+        // alert('po5')
     });
     
     $(document).on('click','#change_gender',function(e){
         e.preventDefault();
-        alert('po6')
+        // alert('po6')
     });
     
     $(document).on('click','#change_price',function(e){
         e.preventDefault();
-        alert('po7')
+        // alert('po7')
     });
     
     $(document).on('click','#change_des',function(e){
         e.preventDefault();
-        alert('po8')
+        // alert('po8')
     });
     
     $(document).on('click','#change_img',function(e){
         e.preventDefault();
-        alert('po9')
+        // alert('po9')
     });
     
     $(document).on('click','#change_link',function(e){
         e.preventDefault();
-        alert('po0')
+        // alert('po0')
     });
 }
 
